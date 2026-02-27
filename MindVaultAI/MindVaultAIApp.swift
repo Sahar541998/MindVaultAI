08a404fefdd7283dc885ea19def7a802f7597f35
saharlevy <le.sahar@gmail.com>
2026-02-28 01:17:15 +0200

    Initial Commit

---
+++ MindVaultAI/MindVaultAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindVaultAIApp.swift
+//  MindVaultAI
+//
+//  Created by Sahar Levy on 28/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct MindVaultAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
